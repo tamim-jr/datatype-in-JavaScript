47b02deda15daf1961b2a2071e3d73dc585e9d41
Tamim Mahmud <internationalinf24@gmail.com>
2022-07-28 23:36:50 +0600

    DataType in JavaScript

---
+++ app.js
@@ -0,0 +1,47 @@
+/*
+Data Type in JavaScript
+
+Primitive data type
+
+Complex data type
+
+primitive data type:
+
+boolean 
+string
+number
+undefined 
+null
+symbol
+
+complex data type
+object
+
+Boolean have two type:
+number one True and number two false
+*/
+
+//find type of data:
+//boolean
+let val = true;
+console.log(typeof val);
+
+//number
+let nam = 55;
+console.log(typeof nam);
+
+//string
+let text = "Hello World";
+console.log(typeof text);
+
+//null
+let findnull = null;
+console.log(typeof findnull); //due to bug it show's object.
+
+//symbol
+const sym = Symbol('food');
+console.log(typeof sym);
+
+//undefine
+const und = undefined;
+console.log(typeof undefined);
